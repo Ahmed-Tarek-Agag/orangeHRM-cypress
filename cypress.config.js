ef1a3aedf71f40cb0a93b36e24d05b266db33a9a
Ahmed Tarek <ahmed.tarek1122@gmail.com>
2026-01-04 19:32:12 +0300

    Reports

--- cypress.config.js
+++ cypress.config.js
@@ -1,15 +1,29 @@
 const { defineConfig } = require("cypress");
+const allureWriter = require("@shelex/cypress-allure-plugin/writer");
 
 module.exports = defineConfig({
+  reporter: "mochawesome",
+  reporterOptions: {
+    reportDir: "cypress/reports",
+    overwrite: false,
+    html: false,
+    json: true
+  },
   e2e: {
-    baseUrl : 'https://opensource-demo.orangehrmlive.com/',
+    baseUrl: 'https://opensource-demo.orangehrmlive.com/',
+
     setupNodeEvents(on, config) {
+      allureWriter(on, config);
       config.env.username = process.env.CYPRESS_USERNAME;
       config.env.password = process.env.CYPRESS_PASSWORD;
       return config;
       // implement node event listeners here
     },
   },
+  env: {
+    allure: true
+  },
+  video: false,
   viewportWidth: 1280,
   viewportHeight: 720
 });
